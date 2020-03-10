9d24e32234bbaa31432e682c0918f071ac1868c4
cossadinha <37553461+cossadinha@users.noreply.github.com>
2020-03-10 15:17:06 -0300

    Alteração de nome de rota

--- src/main/java/com/gft/main/rescontroller/ShowRest.java
+++ src/main/java/com/gft/main/rescontroller/ShowRest.java
@@ -27,7 +27,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @RestController
-@RequestMapping("/showcontroller")
+@RequestMapping("/showrestcontroller")
 public class ShowRest {
 	
 	@Autowired
